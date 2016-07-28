adb490c93e1e7a67e7eff58ce31eb23d12cec53a
artem312 <artemiyka95@gmail.com>
2016-07-28 13:13:13 +0300

    all

---
+++ Assets/Card.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+	private Material back;
+	private Material face;
+
+   int state = 0;
+    float rot = 0f;
+
+    void Start () {
+		transform.Rotate(0,90,0);
+		GetComponent<Renderer>().material = back;
+
+    }
+
+    void Update()
+    {
+        rot += state * 6;
+        transform.Rotate(0, 0, state*6);
+		if (rot == 90) {
+			
+			if (state == 1) {
+				
+				GetComponent<Renderer> ().material = face;
+
+			}
+			else GetComponent<Renderer>().material = back;
+		}
+        if (rot == 180 || rot==0)
+        {
+            state = 0;
+        }
+    }
+    void OnMouseDown()
+    {
+        rotate();
+  	}
+    void rotate()
+    {
+       if(rot==180) { state = -1; }
+        if (rot == 0) { state = 1; }
+    }
+}
---
+++ Assets/Controll.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controll : MonoBehaviour {
+
+    bool it=true;
+    public GameObject Card;
+	 int [][] pole = new int[5][];
+    void Setup()
+    {
+       
+    }
+    void Update()
+    {
+        // RaycastHit hit;
+        if (it == true)
+        {
+            it = false;
+            for (int i = 0; i < 5; i++)
+                for (int k = 0; k < 5; k++)
+                    Instantiate(Card, new Vector3((i - 2) * 2, (k - 2) * 2, -1.9f), Quaternion.identity);
+
+        }
+
+        
+    }
+    
+}
